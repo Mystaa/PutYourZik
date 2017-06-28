4d172bb11ac2a4c0e0379e776061af12a90e0556
Antoine <antoine.thomire@gmail.com>
2017-06-28 18:02:20 +0200

    J'avais oublié un truc

--- src/PutYourZikBundle/Controller/RequestController.php
+++ src/PutYourZikBundle/Controller/RequestController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 
-class UserController extends Controller
+class RequestController extends Controller
 {
     public function requestAction()
     {
